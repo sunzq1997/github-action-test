83d50d30f116f6be5e93e791430a4935ad5b21bd
sunzq <sunzq1997@outlook.com>
2024-01-29 10:46:53 +0800

    add ci.yml

---
+++ src/main/java/com/sunzq/githubactiontest/GithubActionTestApplication.java
@@ -0,0 +1,13 @@
+package com.sunzq.githubactiontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sunzq/githubactiontest/GithubActionTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunzq.githubactiontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
